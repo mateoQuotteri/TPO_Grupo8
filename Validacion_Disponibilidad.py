c29f483f9d5751ba10b2cdde59b5a9282adb7ecb
josemsanchez <josemsanchez@uade.edu.ar>
2026-03-30 20:36:20 -0300

    Agregue el modificar matriz y modificar turno

--- Validacion_Disponibilidad.py
+++ Validacion_Disponibilidad.py
@@ -374,6 +374,93 @@ def eliminar_turno(matriz):
     matriz.pop(indice_eliminado)
     print("\nTurno eliminado con éxito!\n")
 
+def modificar_turno(matriz_turnos, matriz_doctores):
+    dni_buscado = int(input("Ingrese el DNI asociado al turno a modificar: "))
+    while dni_buscado < 10000000 or dni_buscado > 99999999:
+        print("Dato incorrecto")
+        dni_buscado = int(input("Ingrese el DNI asociado al turno a modificar: "))
+    cantidad_turnos = []
+    contador = 1
+    for i in range(1, len(matriz_turnos)):
+        if int(matriz_turnos[i][3]) == dni_buscado:
+            print(contador, "-", matriz_turnos[i])
+            cantidad_turnos.append(i)
+            contador += 1
+    if len(cantidad_turnos) == 0:
+        print("El DNI no tiene turnos asociados")
+        return
+    turno_para_modificar = int(input("Ingrese el numero de turno a modificar: "))
+    while turno_para_modificar < 1 or turno_para_modificar > len(cantidad_turnos):
+        print("Opcion Invalida")
+        turno_para_modificar = int(input("Ingrese el numero de turno a modificar: "))
+    indice_modificado = cantidad_turnos[turno_para_modificar - 1]
+    for i in range(1, len(matriz_turnos)):
+        if int(matriz_turnos[i][3]) == dni_buscado:
+            dia = int(input("Ingrese el dia (1 al 31): "))
+            while dia > 31 or dia < 1:
+                print("Ingrese un dia valido")
+                dia = int(input("Ingrese el dia (1 al 31): "))
+            mes = int(input("Ingrese el mes (1 al 12): "))
+            while mes > 12 or mes < 1:
+                print("Ingrese un mes valido: ")
+                mes = int(input("Ingrese el mes (1 al 12): "))
+            anio = int(input("Ingrese el año: "))
+            while anio < 2026:
+                print("Ingrese un año valido")
+                anio = int(input("Ingrese el año: "))
+            hora_turno = int(input("Ingrese la hora del turno: "))
+            while hora_turno < 1 or hora_turno > 12:
+                print("Dato incorrecto")
+                hora_turno = int(input("Ingrese la hora del turno: "))
+            am_pm_turno = input("Ingrese AM o PM: ").upper()
+            while am_pm_turno != 'AM' and am_pm_turno != 'PM':
+                print("Dato incorrecto")
+                am_pm_turno = input("Ingrese AM o PM: ").upper()
+            contador_doctores = 1
+            print("\nLISTA DE DOCTORES\n")
+            for fila in matriz_doctores[1:]:
+                print(f"{contador_doctores}. Matricula: {fila[1]} - Especialidad: {fila[5]}")
+                contador_doctores += 1
+            opcion_doctores = int(input("\n\nSeleccione al doctor\n"))
+            while opcion_doctores < 1 or opcion_doctores > len(matriz_doctores)-1:
+                print("Opcion Invalida")
+                opcion_doctores = int(input("\n\nSeleccione al doctor\n"))
+            opcion_elegida_doctores = matriz_doctores[opcion_doctores]
+            fecha = str(dia) + '/' + str(mes) + '/' + str(anio)
+            hora = str(hora_turno) + ' ' + am_pm_turno
+            doctor = opcion_elegida_doctores[1]
+            duplicado = False
+            for fila in matriz_turnos[1:]:
+                if fila[1] == fecha and fila[2] == hora and fila[5] == doctor:
+                    duplicado = True
+                    break
+            if duplicado:
+                print("Error: el doctor ya tiene un turno en esa fecha y hora")
+            else:
+                matriz_turnos[indice_modificado] = [contador - 1, fecha, hora, dni_buscado, opcion_elegida_doctores[5], doctor] #aca me falta arreglar que mantenga el indice del turno modificado
+                print("\nDatos modificados con exito\n")
+
+
+def ordenar_matriz(matriz):
+    opciones = matriz[0]
+    print("Ingrese la opcion por la cual ordenar la matriz: ")
+    for i in range(len(opciones)):
+        print(i + 1, "-", opciones[i])
+    while True:
+        opcion = int(input("Ingrese la opcion por la cual desea ordenar la matriz: "))
+        if 1 <= opcion <= len(opciones):
+            columna_a_ordenar = opcion - 1
+            break
+        else:
+            print("Opcion invalida, seleccione una de las disponibles")
+    for i in range(1, len(matriz)):
+        for j in range(1, len(matriz) - 1):
+            if matriz[j][columna_a_ordenar] > matriz[j + 1][columna_a_ordenar]:
+                aux = matriz[j]
+                matriz[j] = matriz[j + 1]
+                matriz[j + 1] = aux
+    return matriz
+
 
 def mostrar_matriz(matriz):
     filas = len(matriz)
@@ -469,6 +556,7 @@ for fila in matriz_turnos:
             print("[2] ABM Doctores.")
             print("[3] ABM Disponibilidad de Doctores.")
             print("[4] ABM Turnos Médicos.")
+            print("[5] Ordenar Matrices.")
             print("---------------------------")
             print("[0] Salir del programa")
             print("---------------------------")
@@ -640,3 +728,8 @@ for fila in matriz_turnos:
                 elif opcion == "3":
                     modificar_turno(matriz_turnos, matriz_doctores)
                     mostrar_matriz(matriz_turnos)
+
+        elif opcion == "5":
+            matriz_ordenada = ordenar_matriz(matriz_pacientes)
+            for fila in matriz_ordenada:
+                print(fila)
